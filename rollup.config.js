4fb11985400e919a133e6fdaecd65425a5dec72f
samwangdd <wyysammore@gmail.com>
2021-06-03 17:02:41 +0800

    chore: rollup

---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+// rollup.config.js
+export default {
+  input: 'src/main.ts',
+  output: {
+    file: 'lib/bundle.js',
+    format: 'cjs'
+  }
+};
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+// src/main.ts
+export default function () {
+  console.log('foo');
+}
